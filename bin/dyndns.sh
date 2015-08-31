960a7a7888292e23640acc7607cecec15504de59
鲁 <jiawu.lu@gmail.com>
2015-08-31 16:08:17 +0800

    Update dyndns.sh
    update

--- bin/dyndns.sh
+++ bin/dyndns.sh
@@ -2,4 +2,4 @@
 
 #put it to /etc/network/if-up.d/
 myip=`ifconfig| grep "255.255." | awk '{print $2 }' | awk -F: '{print $2}'`
-wget --user=jiawulu --password=home301 "http://www.3322.org/dyndns/update?system=dyndns&hostname=jiawulu.3322.org&myip=$myip" >/dev/null 2>/dev/null
+wget --user=jiawulu --password=xxxx "http://www.3322.org/dyndns/update?system=dyndns&hostname=jiawulu.3322.org&myip=$myip" >/dev/null 2>/dev/null
